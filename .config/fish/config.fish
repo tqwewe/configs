18ac4c3c359f9fb1c3e16798b41164863e1cc652
Ari Seyhun <ariseyhun@live.com.au>
2022-05-19 13:20:29 +0930

    add fish and helix configs and remove bashrc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -29,18 +29,36 @@ function dev
     node ~/.scripts/dev.js | bash
 end
 
+function wslip
+    ip -4 addr show eth0
+end
+
+function fish_user_key_bindings
+    bind \e\[1\;5D backward-word
+    bind \e\[1\;5C forward-word
+    bind \b backward-kill-word
+end
+
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
+fish_add_path /usr/share/swift/usr/bin
+
+set -gx GPG_TTY $(tty)
 
 alias cat "batcat"
 alias ls "exa --long --group-directories-first --no-permissions --no-user"
 alias ps "procs"
 alias vim "nvim"
 
-cd ~/
-
 if status is-interactive
-    # Commands to run in interactive sessions can go here
 end
 
 starship init fish | source
+
+# Wasienv
+export WASIENV_DIR="/home/ari/.wasienv"
+[ -s "$WASIENV_DIR/wasienv.sh" ] && source "$WASIENV_DIR/wasienv.sh"
+
+# Wasmer
+export WASMER_DIR="/home/ari/.wasmer"
+[ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
